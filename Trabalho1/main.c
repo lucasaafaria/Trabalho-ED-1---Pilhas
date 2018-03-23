f9d5f6da59d547e19eb0d00e1f7226c736b95bb4
farialucas <lucasaafaria@gmail.com>
2018-03-23 08:18:36 -0300

    Main criada

---
+++ Trabalho1/main.c
@@ -0,0 +1,13 @@
+/*------------------------- Trabalho 1 - Pilhas -------------------------
+*		@autores: Lucas de Almeida Abreu Faria, Mateus Berardo de Souza Terra
+*	 @matriculas: 17/0016668 ; 17/0018806
+*    @disciplina: Estruturas de Dados
+*     @professor: Eduardo Alchieri
+*/
+
+#include <stdio.h>
+
+int main(){
+
+	return 0;
+}
