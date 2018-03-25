627c39d626e0d0e01d5ebfe11a4ba36f597d681e
farialucas <lucasaafaria@gmail.com>
2018-03-25 08:28:48 -0300

    Criadas funções de validação e entrada de dados

--- Trabalho1/src/inputs.c
+++ Trabalho1/src/inputs.c
@@ -8,10 +8,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "inputs.h"
 #include "stack.h"
+#include "queue.h"
 
-	/* Funções de comparação */
+/* Funções de comparação */
 	int isParenthesis(char * dado){
 		if((*dado) == '(')
 			return OPEN_PAR;
@@ -31,9 +33,45 @@
 			return 1;
 		return 0;
 	}
-	
-	int isSpace(char * dado){
-		if((*dado) == ' ')
-			return 1;
-		return 0;
+
+	void getExpression(t_queue * queue){
+		char item = '\0';
+		int number = 0;
+
+		while(item != '\n'){
+			scanf("%c", &item);
+			while(isNumber(&item)){
+				number += (number * 10) + (item - '0');
+				scanf("%c", &item);
+				if(!isNumber(&item)){
+					pushQueue(queue, &number, t_int);
+					number = 0;
+				}
+			}
+			if(item != ' ' && item != '\n')
+				pushQueue(queue, &item, t_char);
+		}
+	}
+
+	int validExpression(t_queue * queue){
+		char * element;
+		t_stack * stack = newStack();
+		t_queueElement * current = queue->inicio;
+
+		while(current != NULL){
+			if(isParenthesis((char *)current->dado) == OPEN_PAR){
+				element = (char *) current->dado;
+				pushStack(element, stack, t_char);
+			}else if(isParenthesis((char *)current->dado) == CLOSE_PAR){
+				if(emptyStack(stack)) /* If true, there aren't any '(' in the stack. */
+					return 0;	
+				element = (char *) popStack(stack);	
+			}else if(!isOperator((char *)current->dado) && !isNumber((char *)current->dado))
+				return 0;
+			current = current->proximo;
+		}
+
+		if(!emptyStack(stack)) /* If true, there are still '(' in the stack. More '(' than ')' */
+			return 0;
+		return 1;
 	}
--- Trabalho1/src/inputs.h
+++ Trabalho1/src/inputs.h
@@ -11,4 +11,8 @@
 	extern int isParenthesis(char * dado);
 	extern int isSpace(char * dado);
 
+	/* Definição de funções de entrada e tratamento de dados */
+	extern void getExpression(t_queue * queue);
+	extern int validExpression(t_queue * queue);
+
 #endif
