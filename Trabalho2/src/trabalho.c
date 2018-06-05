8dbc794c3970e2e896cd10b694e0ce809a006b6c
farialucas <lucasaafaria@gmail.com>
2018-06-05 16:05:32 -0300

    versão final (quem sabe)

--- Trabalho2/src/trabalho.c
+++ Trabalho2/src/trabalho.c
@@ -6,6 +6,7 @@
 #define ONE_OVER_BILLION 1E-9 
 
 
+/* DEFINIÇÃO DE STRUCTS PARA A CRIAÇÃO DA LISTA */
 typedef struct elemento{
     char simbolo;
     char codigo[10];
@@ -17,19 +18,13 @@ typedef struct lista{
     t_elemento *fim;
 } t_lista;
 
+/* FUNÇÕES REFERENTES AO USO DA LISTA */
 t_lista * novaLista(){
     t_lista * l = (t_lista*) malloc(sizeof(t_lista));
     l->inicio = NULL;
     l->fim = NULL;
     return l;
 }
-
-typedef struct no{
-    char caractere;
-    struct no* ponto;
-    struct no* traco;
-} t_no;
-
 
 void insereInicio(t_lista *lista, char simbolo, char * codigo){
     t_elemento * elemento = (t_elemento*) malloc(sizeof(t_elemento));
@@ -43,7 +38,7 @@ void insereInicio(t_lista *lista, char simbolo, char * codigo){
 
 t_lista * dicionario(){
     int lido = 1;
-    char simbolo, codigo[15];
+    char simbolo, codigo[10];
     t_lista * lista = novaLista();
     FILE * morse = fopen("morse.txt", "r");
     while(lido != -1){
@@ -68,27 +63,35 @@ char procura(char * codigo, t_lista * l){
 }
 
 void decodificaLista(t_lista * tradutor){
-	int lido = 1;
+    int lido = 1;
     char codigo[10];
-	FILE * mensagem = fopen("mensagem.txt", "r");
-
-	while(lido != -1){
-		lido = fscanf(mensagem, "%s ", codigo);
-		if(lido != -1){
-			if(strcmp(codigo, "/") != 0)
-				putchar(procura(codigo, tradutor));
-			else
-				putchar(' ');
-		}
-	}
-	fclose(mensagem);
+    FILE * mensagem = fopen("mensagem.txt", "r");
+    while(lido != -1){
+        lido = fscanf(mensagem, "%s ", codigo);
+        if(lido != -1){
+            if(strcmp(codigo, "/") != 0)
+                putchar(procura(codigo, tradutor));
+            else
+                putchar(' ');
+        }
+    }
+    fclose(mensagem);
 }
 
+/* DEFINIÇÃO DE STRUCT PARA A CRIAÇÃO DA ÁRVORE */
+typedef struct no{
+    char caractere;
+    struct no* ponto;
+    struct no* traco;
+} t_no;
+
+/* FUNÇÕES REFERENTES AO USO DA ÁRVORE */
 t_no* criarNo(char dado){
     t_no* novo = (t_no*) malloc(sizeof(t_no));
     novo->caractere = dado;
     return novo;
 }
+
 void inserirNo(t_no* raiz, char *codigo, char letra){
     if(codigo[1]=='\0'){
         if(codigo[0]=='.'){
@@ -120,7 +123,7 @@ void inserirNo(t_no* raiz, char *codigo, char letra){
 
 t_no* montarArvore(){
     t_no *raiz = criarNo(' ');
-    FILE *arvore = fopen( "./morse.txt", "r" );
+    FILE *arvore = fopen( "morse.txt", "r" );
     char codigo[9], letra;
     while(!feof(arvore)){
         fscanf(arvore, "%c %s\n", &letra, codigo);
@@ -143,38 +146,40 @@ char traduzir(t_no *raiz, char *codigo){
 }
 
 int main(){
-    t_no *raiz = montarArvore();
     struct timespec requestStart, requestEnd;
-    clock_gettime(CLOCK_REALTIME, &requestStart);
-    FILE *mensagem = fopen( "./mensagem.txt", "r" );
-<<<<<<< HEAD
-    char codigo[9],a;/*
-=======
-    char codigo[9],a;
->>>>>>> 385f1538f3784a4bf0b0983f0627491c281b0ef1
-    a=fgetc(mensagem);
+    char codigo[9], c;
+
+    /* Criação da árvore e da lista encadeada */
+    t_no * raiz = montarArvore();
+    t_lista * tradutor = dicionario();
+
+    /* Para imprimir a mensagem original em Morse */
+    FILE *mensagem = fopen( "mensagem.txt", "r" );
     while(!feof(mensagem)){
-        putchar(a);
-        a=fgetc(mensagem);
-    }
-    fseek(mensagem, 0, SEEK_SET);
-    while(fscanf(mensagem, "%s ", codigo) != -1){
-        putchar(traduzir(raiz, codigo));
+        c = fgetc(mensagem);
+        if(!feof(mensagem))
+            putchar(c);
     }
     fclose(mensagem);
-    clock_gettime(CLOCK_REALTIME, &requestEnd);
-    // Calculate time it took
-    double accum = ( requestEnd.tv_sec - requestStart.tv_sec )+( requestEnd.tv_nsec - requestStart.tv_nsec )*ONE_OVER_BILLION;;
-    printf( "%lf\n", accum );
-    
+    putchar('\n');
+
+    /* Tradução usando a árvore*/
     clock_gettime(CLOCK_REALTIME, &requestStart);
-    t_lista * tradutor = dicionario();
-	decodificaLista(tradutor);
-	clock_gettime(CLOCK_REALTIME, &requestEnd);
-    // Calculate time it took
-    accum = ( requestEnd.tv_sec - requestStart.tv_sec )+( requestEnd.tv_nsec - requestStart.tv_nsec )*ONE_OVER_BILLION;;
-    printf( "%lf\n", accum );    
+    mensagem = fopen( "mensagem.txt", "r" );
+    while(fscanf(mensagem, "%s ", codigo) != -1)
+        putchar(traduzir(raiz, codigo));
+    fclose(mensagem);
+    clock_gettime(CLOCK_REALTIME, &requestEnd);
+    double accum1 = ( requestEnd.tv_sec - requestStart.tv_sec )+( requestEnd.tv_nsec - requestStart.tv_nsec )*ONE_OVER_BILLION;
+    printf( "\n\nTempo de execução para a árvore: %lfs\n\n", accum1 );
+    
+    /* Tradução usando a lista encadeada */
+    clock_gettime(CLOCK_REALTIME, &requestStart);
+    decodificaLista(tradutor);
+    clock_gettime(CLOCK_REALTIME, &requestEnd);
+    double accum2 = ( requestEnd.tv_sec - requestStart.tv_sec )+( requestEnd.tv_nsec - requestStart.tv_nsec )*ONE_OVER_BILLION;
+    printf( "\n\nTempo de execução para a lista: %lfs\n", accum2 );
+    printf( "\nDiferença entre os tempos de execução: %lfs\n", accum2-accum1 );    
 
     return 0;
 }
-
